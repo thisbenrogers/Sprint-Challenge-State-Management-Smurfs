d294f6239163bb46e52eec1af7ea032d909239e4
Ben <thisbenrogers@gmail.com>
2019-08-16 11:03:49 -0500

    :building_construction: Adds reducer

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,17 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
+import axios from 'axios';
 
 import SmurfList from './SmurfList';
+import { SmurfContext } from '../contexts/SmurfContext';
 
 import "./App.css";
-class App extends Component {
-  render() {
+
+const App = () => {
+  let smurfs = [];
+  axios.get('http://localhost:3333/smurfs')
+    .then(res => {
+      smurfs = [...res.data];
+      console.log("smurfs in axios: ", smurfs);
+    })
+
+
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Context and useReducer</h1>
         <SmurfList />
       </div>
     );
-  }
 }
 
 export default App;
